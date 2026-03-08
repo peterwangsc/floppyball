6dffc94fd7f9aae271ad8d5855287189245922b0
Peter Wang <peterwangsc@gmail.com>
2026-03-08 05:42:33 -0700

    Triangle

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./lib/ascii.ts";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./components/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>,
 );
